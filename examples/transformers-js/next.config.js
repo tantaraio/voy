87ea67a3abc43b5b00092dfbc11ca15eb3ee41c7
Daw-Chih Liou <dawochih.liou@gmail.com>
2023-07-10 21:10:40 +0200

    fix: resize embeddings

---
+++ examples/transformers-js/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: function (config, options) {
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
